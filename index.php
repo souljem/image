22b4112bd4bf692c009ba4a110c9ed771c064cfb
souljem <souljem@gmail.com>
2018-08-05 18:08:25 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "
+    <html lang='en'>
+    <head>
+    
+    Create a Part in Database
+    <br>
+</head>
+    <body>
+        
+            <br>
+             
+        <form action=\"tester.php\" enctype=\"multipart/form-data\"  method=\"POST\" >
+        
+        <input type=\"file\" name=\"pic1\" size=\"25\" />
+            <input type=\"submit\" value=\"Send File\"  />
+            </form>    
+
+    </body>
+</html>
+";
+
+
+
+
+
+?>
